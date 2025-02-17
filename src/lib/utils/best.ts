4c5bcf2d35d17b606e8251a1c95269d4ddc40d36
Kim, Hyeonseo <dodok8@gmail.com>
2025-02-17 23:49:37 +0900

    ✨ add: getPotentialCharts

--- src/lib/utils/best.ts
+++ src/lib/utils/best.ts
@@ -25,3 +25,27 @@ export function getBest30Average(charts: ChartInfo[]): number {
 		? charts.reduce((sum, chart) => sum + chart.rating, 0) / charts.length
 		: 0;
 }
+
+export function getPotentialCharts(scores: Score[], targetRating: number): ChartInfo[] {
+	return scores
+		.flatMap((song): ChartInfo[] =>
+			song.charts
+				.filter(
+					(chart) =>
+						// 현재 점수가 0이 아니고
+						chart.score !== 0 &&
+						// 난이도가 목표 레이팅 - 3.6 이상인 차트만 선택
+						chart.difficultyDecimal >= targetRating - 3.6
+				)
+				.map((chart) => ({
+					songId: song.id,
+					songTitle: song.title_localized.default,
+					imageUrl: song.imageUrl,
+					difficultyLevel: chart.difficultyLevel,
+					difficultyDecimal: chart.difficultyDecimal,
+					score: chart.score ?? 0,
+					rating: chart.rating ?? 0
+				}))
+		)
+		.sort((a, b) => b.rating - a.rating);
+}
